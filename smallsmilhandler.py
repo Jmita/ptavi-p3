ef63b53724e2c09a4e21a5dd13f22c52a71a3ad5
Juan Manuel Yuste Carretero <juanmis@zeta05.aulas.gsyc.es>
2014-10-13 09:51:33 +0200

    fichero subido

---
+++ smallsmilhandler.py
@@ -0,0 +1,63 @@
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+
+
+class SmallSMILHandler(ContentHandler):
+
+    def __init__(self):
+        self.lista = []
+
+    def startElement(self, name, attrs):
+        if name == "root-layout":
+            self.root_layout = {}
+            self.root_layout['width'] = attrs.get('width', "")
+            self.root_layout['height'] = attrs.get('height', "")
+            self.root_layout['background_color'] = (
+            attrs.get('background-color', ""))
+            self.lista.append([name, self.root_layout])
+
+        elif name == "region":
+            self.region = {}
+            self.region['id'] = attrs.get('id', "")
+            self.region['top'] = attrs.get('top', "")
+            self.region['bowttom'] = attrs.get('bowttom', "")
+            self.region['left'] = attrs.get('left', "")
+            self.region['right'] = attrs.get('right', "")
+            self.lista.append([name, self.region])
+
+        elif name == "img":
+            self.img = {}
+            self.img['src'] = attrs.get('src', "")
+            self.img['region'] = attrs.get('region', "")
+            self.img['begin'] = attrs.get('begin', "")
+            self.img['dur'] = attrs.get('dur', "")
+            self.lista.append([name, self.img])
+
+        elif name == "audio":
+            self.audio = {}
+            self.audio['src'] = attrs.get('src', "")
+            self.audio['begin'] = attrs.get('begin', "")
+            self.audio['dur'] = attrs.get('dur', "")
+            self.lista.append([name, self.audio])
+
+        elif name == "textstream":
+            self.textstream = {}
+            self.textstream['src'] = attrs.get('src', "")
+            self.textstream['region'] = attrs.get('region', "")
+            self.lista.append([name, self.textstream])
+
+    def get_tags(self):
+
+        return self.lista
+
+
+if __name__ == "__main__":
+    """
+    Programa principal
+    """
+    parser = make_parser()
+    SSHandler = SmallSMILHandler()
+    parser.setContentHandler(SSHandler)
+    parser.parse(open('karaoke.smil'))
+    data = SSHandler.get_tags()
+    print data
